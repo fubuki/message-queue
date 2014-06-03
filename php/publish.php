568eec6420b84781b8ea91ded87e2bf55b69ac28
fubuki <fubuki@users.noreply.github.com>
2014-06-03 19:44:24 +0800

    初始化一個PHP beanstalk 推送端

---
+++ php/publish.php
@@ -0,0 +1,13 @@
+<?php
+
+
+require_once('pheanstalk_init.php');
+
+$pheanstalk = new Pheanstalk_Pheanstalk('127.0.0.1');
+
+$pheanstalk
+  ->useTube('testtube')
+  ->put("job payload goes here\n");
+
+
+$pheanstalk->getConnection()->isServiceListening(); // true or false
