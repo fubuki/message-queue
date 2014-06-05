ceb214f57881eed40bdd7ec65f6d988b05232265
fubuki <fubuki@users.noreply.github.com>
2014-06-05 21:51:12 +0800

    讓php透過beanstalk與nodejs平台溝通的方案測試完成。

--- nodejs/sub.js
+++ nodejs/sub.js
@@ -1,15 +1,17 @@
 var bs = require('nodestalker'),
     client = bs.Client(),
-    tube = 'test_tube';
+    tube = 'testtube';
 
 client.watch(tube).onSuccess(function(data) {
     function resJob() {
         client.reserve().onSuccess(function(job) {
-            console.log('reserved', job);
+
+            var json = JSON.parse(job.data);
+            console.log("id:"+json.id+"_"+"data:"+json.data);
 
             client.deleteJob(job.id).onSuccess(function(del_msg) {
-                console.log('deleted', job);
-                console.log('message', del_msg);
+                //console.log('deleted', job);
+                //console.log('message', del_msg);
                 resJob();
             });
         });
--- php/publish.php
+++ php/publish.php
@@ -5,9 +5,13 @@ require_once('../pheanstalk/pheanstalk_init.php');
 
 $pheanstalk = new Pheanstalk_Pheanstalk('127.0.0.1');
 
+$messgae = array("id"=>123456,
+				"data"=>"測試用訊息");
+
+
 $pheanstalk
   ->useTube('testtube')
-  ->put("job payload goes here\n");
+  ->put(json_encode($messgae));
 
 
 $pheanstalk->getConnection()->isServiceListening(); // true or false
